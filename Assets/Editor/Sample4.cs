ac8609fa955792283c413a89f815f844465a77cd
tsasaki <sasaki.takashi.9s7@jp.nssol.nipponsteel.com>
2019-08-19 13:06:12 +0900

    add Sample4

--- Assets/Editor/Sample4.cs
+++ Assets/Editor/Sample4.cs
@@ -6,33 +6,39 @@ using UnityEditor.UIElements;
 
 public class Sample4 : EditorWindow
 {
-    [MenuItem("Window/UIElements/Sample4")]
+    [MenuItem("UIElementsSamples/Sample4")]
     public static void ShowExample()
     {
         Sample4 wnd = GetWindow<Sample4>();
         wnd.titleContent = new GUIContent("Sample4");
     }
 
+    private Label label;
+    private Image image;
+    private SliderInt slider;
 
     public void OnEnable()
     {
-        // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
-
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
 
         // Import UXML
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/Sample4.uxml");
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/Sample4.uss");
         VisualElement labelFromUXML = visualTree.CloneTree();
+        labelFromUXML.styleSheets.Add(styleSheet);
         root.Add(labelFromUXML);
 
-        // A stylesheet can be added to a VisualElement.
-        // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/Sample4.uss");
-        VisualElement labelWithStyle = new Label("Hello World! With Style");
-        labelWithStyle.styleSheets.Add(styleSheet);
-        root.Add(labelWithStyle);
+        this.label = root.Q<Label>("label");
+        this.image = root.Q<Image>("image");
+        this.slider = root.Q<SliderInt>("slider");
+
+        slider.RegisterValueChangedCallback<int>(Slider_ValueChanged);
+    }
+
+    private void Slider_ValueChanged(ChangeEvent<int> evt)
+    {
+        this.label.text = $"page {evt.newValue}/{slider.highValue}";
+        var backgroundTexture = Resources.Load<Texture2D>($"img_{evt.newValue}");
+        this.image.style.backgroundImage = backgroundTexture;
     }
 }
